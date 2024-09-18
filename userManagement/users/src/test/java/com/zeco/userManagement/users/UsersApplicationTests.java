e0970f3eaf688b5932546f52cffd948b0e6b6553
nkemahJunior <77687679+nkemahjunior@users.noreply.github.com>
2024-09-18 18:41:37 +0100

    Update UsersApplicationTests.java

--- userManagement/users/src/test/java/com/zeco/userManagement/users/UsersApplicationTests.java
+++ userManagement/users/src/test/java/com/zeco/userManagement/users/UsersApplicationTests.java
@@ -1,13 +1,13 @@
 package com.zeco.userManagement.users;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UsersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
