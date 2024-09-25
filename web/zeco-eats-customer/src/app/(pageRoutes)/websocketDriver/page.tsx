492beebd56d44f6f00d2fcde43a251f0a18ffe88
nkemahJunior <77687679+nkemahjunior@users.noreply.github.com>
2024-09-25 16:26:33 +0100

    arrange repo (#7)

---
+++ web/zeco-eats-customer/src/app/(pageRoutes)/websocketDriver/page.tsx
@@ -0,0 +1,11 @@
+import WebsocketTest from "@/app/websockets/WebsocketTest";
+
+ 
+ 
+export default function () {
+    return (
+        <div>
+            <WebsocketTest/>
+        </div>
+    );
+}
