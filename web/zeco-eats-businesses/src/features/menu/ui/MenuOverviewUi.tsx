e76cb401d8e829b94b8bf2429d0e0cee6528f9cd
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2024-12-24 16:33:45 +0100

    feat: menu overview - menu hours

--- web/zeco-eats-businesses/src/features/menu/ui/MenuOverviewUi.tsx
+++ web/zeco-eats-businesses/src/features/menu/ui/MenuOverviewUi.tsx
@@ -1,9 +1,13 @@
+import Line from "@/shared/components/Line";
+import MenuHours from "../components/overview/MenuHours";
 import MenuTitle from "../components/overview/MenuTitle";
 
 export default function MenuOverviewUi() {
   return (
     <div className=" space-y-8">
-      <MenuTitle />
+          <MenuTitle />
+          <MenuHours />
+          <Line/>
     </div>
   );
 }
